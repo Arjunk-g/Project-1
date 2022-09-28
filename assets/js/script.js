d8ae4429e387766be959cbb74d9c62d753338f2b
Wookie Choi <hyunwook2010@gmail.com>
2022-09-28 14:03:33 -0400

    encapsulated gold api code into a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
-var goldApiUrl = "https://www.goldapi.io/api/XAU/USD";
+function getGoldApi() {
+    var goldApiUrl = "https://www.goldapi.io/api/XAU/USD?per_page=5";
 
-var myHeaders = new Headers();
-myHeaders.append("x-access-token", "goldapi-1un118l8lqe0hm-io");
-myHeaders.append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("x-access-token", "goldapi-1un118l8lqe0hm-io");
+    myHeaders.append("Content-Type", "application/json");
+    
+    fetch(goldApiUrl, {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+    })
+    .then(function (response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    });
+}
 
-fetch(goldApiUrl, {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-})
-.then(function (response) {
-    return response.json()
-})
-.then(function (data) {
-    console.log(data)
-});
+getGoldApi();
