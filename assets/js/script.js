68a8f6676923c8d6f3e0e7460227504615efabc1
Wookie Choi <hyunwook2010@gmail.com>
2022-09-30 19:55:29 -0400

    added conditional to check for karat decision :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //Global Variables
+var amountMetalFrom = document.querySelector(".amountMetalFrom");
 var amountCountryFrom = document.body.querySelector(".amountCountryFrom");  //Variable holding the input amount to be converted
 var countrySelection = document.body.querySelector(".countrySelection");  //Variable to select country
 var conversionAmount  = document.body.querySelector(".conversionAmount");
-var btnConvert = document.body.querySelector(".btnConvert");
+var btnConvertG = document.body.querySelector(".btnConvertG");
 var btnStartOver = document.body.querySelector(".btnStartOver");
 var messageHandler = document.body.querySelector("#messageHandler");
 var amountBTC = document.body.querySelector("#amountBTC");
@@ -65,8 +66,8 @@ function convertCurrencyBTC() {
 
 btnConvertBTC.addEventListener("click",convertCurrencyBTC);
 
-function getGoldApi(from, to, amount) {
-    var amountCountry = document.getElementById("amountCountryFrom");
+
+function getGoldApi(from, to, amount, metal) {
     var goldApiUrl = "https://www.goldapi.io/api/" + from + "/" + to + "/";
     var myHeaders = new Headers();
     var goldData;
@@ -84,29 +85,46 @@ function getGoldApi(from, to, amount) {
         return response.json();
     })
     .then(function (data) {
-        //in 24k
+        //conditional for karats
+        if(metal === "18k") {
+            goldData = data.price_gram_18k;
+        }else if(metal === "20k") {
+            goldData = data.price_gram_20k;
+        }else if(metal === "21k") {
+            goldData = data.price_gram_21k;
+        }else if(metal === "22k") {
+            goldData = data.price_gram_22k;
+        }else if(metal === "24k") {
             goldData = data.price_gram_24k;
-        // goldData = data.price_gram_24k;
+        }
+
+        //goldConvert equals karats x amount in grams
         var goldConvert = goldData*amount;
         //get result element
         var resultText = document.getElementById("result");
         //result text shown equals goldData x amountFrom
+        if(to !== "BTC") {
+            goldConvert = goldConvert.toFixed(2);
+        }
         resultText.value = goldConvert;
     });
 }
 
 function getGoldConversion() {
-    //
-    var goldFromOption = document.getElementById("countryFromOption");
-    var countryToOption = document.getElementById("countryToOption");
-    var amountCountry = document.getElementById("amountCountryFrom");
+    //make variable to get elements
+    var goldFromOption = document.getElementById("metalFromOption"); 
+    var countryToOption = document.getElementById("countryToOptionG");
+    var amountMetal = document.getElementById("amountMetalFrom");
+    var karatFrom = document.getElementById("karatFrom");
 
-    //
-    var goldFromText = goldFromOption.options[goldFromOption.selectedIndex].text;
+    //set text from grabbed elements
+    var goldFromText = goldFromOption.options[goldFromOption.selectedIndex].text; 
     var countryToText = countryToOption.options[countryToOption.selectedIndex].text;
-    var amountCountryText = amountCountry.value;
+    var amountMetalText = amountMetal.value;
+    var karatFromText = karatFrom.value;
 
-    getGoldApi(goldFromText, countryToText, amountCountryText);
+    //call getGoldApi with parameters (metal dropdown, currency dropdown, metal in grams, karat of metal)
+    getGoldApi(goldFromText, countryToText, amountMetalText, karatFromText); 
 }
 
-btnConvert.addEventListener("click", getGoldConversion);
+btnConvertG.addEventListener("click", getGoldConversion);
